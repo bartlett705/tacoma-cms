9818941ea4da1c3a68bfa3a986c78fdfec53bf75
Ahmad K <lance@codemosey.net>
2017-01-18 23:31:44 -0800

    Makes tag-tabs colored based on tag length.

--- common/components/BlogPostViewer.jsx
+++ common/components/BlogPostViewer.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import marked from 'marked';
 
+const tagColors = [
+  'skyblue',
+  '#66ab66',
+  'DarkSeaGreen',
+  'Thistle',
+  'Salmon',
+  'Gold',
+  'Peru',
+  'LightSteelBlue',
+  'Tomato',
+];
+
+function getTagColor(tag) {
+  return tagColors[tag.length % tagColors.length];
+}
+
 const BlogPostViewer = ({ postData }) => {
   const { date = Date.now(), title = 'Title Redacted', tags = ['es6', 'node', 'react', 'react-redux'], body = 'Body Omitted.' } = postData;
   return (
     <div>
       <div id="tags">
         {tags.map(tag => (
-          <a className="tag" key={tag} href="#">
+          <a className="tag" key={tag} href="#" style={{backgroundColor: getTagColor(tag)}}>
             {tag}
           </a>
         ))}
